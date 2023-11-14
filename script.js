606f7267228099b33757eae63d968eb895cb3a7b
jwc1 <cheyu36@gmail.com>
2023-11-14 10:53:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let map;
+// initMap is now async
+async function initMap() {
+    // Request libraries when needed, not in the script tag.
+    const { Map } = await google.maps.importLibrary("maps");
+    // Short namespaces can be used.
+    map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+        mapId: '23590b1347872a11'
+    });
+} 
+
+initMap();
